9991e1058338b6b03ff86c429b0fe69a8b6d6ebb
shakh <goo.eax@gmail.com>
2023-07-25 19:26:58 +0600

    task/list: add fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [ './src/**/*.{js,ts,jsx,tsx}' ],
   theme: {
     extend: {
       colors: {
         primary: '#EF4444',
         secondary: '#E7D0AF',
-        accent: '#FFA230'
-      }
+        accent: '#FFA230',
+      },
+      fontFamily: {
+        sans: [ '"SF Pro Display"', ...defaultTheme.fontFamily.sans ],
+      },
     },
   },
-  plugins: [require('tailwindcss-animated'), require("@tailwindcss/typography"), require("prettier-plugin-tailwindcss"), require("tailwindcss-safe-area")],
+  plugins: [
+    require('tailwindcss-animated'),
+    require('@tailwindcss/typography'),
+    require('prettier-plugin-tailwindcss'),
+    require('tailwindcss-safe-area'),
+  ],
 };
