3c5daa7170b81105f561d3797254eaf60d85df69
shakh <goo.eax@gmail.com>
2023-07-26 16:29:57 +0600

    task/list: update components preview

--- src/App.tsx
+++ src/App.tsx
@@ -14,35 +14,43 @@ import {
 
 const components = [
   'List',
+  'PlanPicker',
   'Chart',
   'MoneyBack',
   'Reviews',
-  'PlanPicker',
   'ProgressBar',
   'Loader',
   'LoaderSet',
   'Rating',
 ];
 
+const implementedComponents = ['List', 'PlanPicker'];
+
 export function App() {
-  const selectedComponent = useSignal('List');
+  const selectedComponent = useSignal('PlanPicker');
 
   return (
-    <main class="h-screen touch-pan-y overflow-x-hidden m-auto px-5 pt-10 pr-40 flex justify-center">
-      <div class='flex gap-20 px-10 w-full items-center justify-center'>
-        <div class='flex flex-col gap-2 mb-10 pt-10'>
+    <main class="h-screen touch-pan-y overflow-x-hidden m-auto sm:px-5 py-10 flex justify-center">
+      <div class='flex flex-col gap-12 sm:px-10 w-full items-center'>
+        <select
+          value={selectedComponent}
+          class='p-3 rounded-md outline-none font-mono appearance-none text-center text-lg'
+          onChange={(e: any) => {
+            selectedComponent.value = e.target.value;
+          }}
+        >
+          <option value="">-- Select your component --</option>
           { components.map(component => (
-            <span
+            <option
               key={component}
-              class={'cursor-pointer hover:opacity-100 ' + (component === selectedComponent.value ? 'font-bold opacity-100' : 'opacity-70')}
-              onClick={() => {
-                selectedComponent.value = component;
-              }}
+              value={component}
+              selected={component === selectedComponent.value}
+              disabled={!implementedComponents.includes(component)}
             >
               { component }
-            </span>
+            </option>
           )) }
-        </div>
+        </select>
 
         <div class='max-w-sm w-full px-4 py-9 bg-white rounded-lg'>
           { selectedComponent.value === 'List' && <ListComponent title={list.title} items={list.items as any} /> }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,16 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [ './src/**/*.{js,ts,jsx,tsx}' ],
   theme: {
     extend: {
+      opacity: {
+        8: '.08',
+      },
       borderRadius: {
         'lg': '32px',
+        'md': '16px',
       },
       fontSize: {
         '2xl': [ '28px', '36px' ],
@@ -15,7 +20,11 @@ module.exports = {
         secondary: '#E7D0AF',
         accent: '#FFA230',
         gray: {
+          500: '#8f8f8f',
           800: '#111111',
+        },
+        red: {
+          400: '#f8545d',
         },
       },
       fontFamily: {
