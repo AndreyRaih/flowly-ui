d56ce1571cf61521cb51a86bc6d907bd95ef02b2
Andrey Raih <andreyraih@yandex.ru>
2023-08-09 22:30:17 +0800

    add customer-review task

---
+++ src/tasks/customer-review/README.md
@@ -0,0 +1,4 @@
+# Flowly UI: Customer Review Component
+
+Ссылка на Figma: https://www.figma.com/file/L99aNrt1xCvKc2htYAeEV7/UI-Components?type=design&node-id=203-54&mode=design&t=z1mOQnliTxFzMOKl-4
+
---
+++ src/tasks/customer-review/index.tsx
@@ -0,0 +1,15 @@
+type CustomerReviewProps = {
+  imgUrl: string
+  review: {
+    rate: number
+    text: string
+    author: string
+    date: string
+  }
+}
+
+export function CustomerReview(props: CustomerReviewProps) {
+  return (
+    <div />
+  );
+}
